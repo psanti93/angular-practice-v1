585736ab2990305f49073270964ecae09f4a2dbb
Paul Santiago <paulsantiago282@gmail.com>
2023-07-21 16:36:24 -0400

    show examples of selectors

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // example of element selector
+ // selector: '[app-servers]', // example of an attribute selector
+ // selector: '.app-servers',
   template: '<app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
