d234881936bffc0fd8364f36fd96ccad49a6db64
Paul Santiago <paulsantiago282@gmail.com>
2023-07-21 18:29:31 -0400

    Add examples fo rtwo way binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
-
+  serverCreationStatus = 'No server was created';
+  serverName = ""
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
     }, 2000)
   }
+
+  // event binding example
+  onCreateServer() {
+    this.serverCreationStatus = "Server was Created!"
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
+  }
+
 
 }
