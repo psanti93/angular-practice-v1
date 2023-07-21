20c9e8d28b8489e40655a5c81560aa9d81880fb5
Paul Santiago <paulsantiago282@gmail.com>
2023-07-21 18:50:30 -0400

    Putting it all together

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,13 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
-  serverName = "test server"
+  allowNewServer = false; //property binding
+  serverCreationStatus = 'No server was created'; //string interpolation
+  serverName = "Test Server" //two way binding
+  someMessage = "TYPE HERE" // one way binding
   constructor() {
+    // property binding example
     setTimeout(()=>{
       this.allowNewServer = true;
     }, 2000)
+  }
+
+
+  // Event binding
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! server name is ' + this.serverName;
+  }
+
+  // Event Binding - one way
+  onUpdateServerName(event: Event){
+    this.someMessage = (<HTMLInputElement>event.target).value;
   }
 
 }
