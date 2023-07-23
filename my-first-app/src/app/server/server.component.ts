64351ca38899b36bc85ac7a8ca46ca70caf9cffa
Paul Santiago <paulsantiago282@gmail.com>
2023-07-23 12:49:34 -0400

    add example of using ng-style

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -9,8 +9,16 @@ import { Component } from "@angular/core";
 export class ServerComponent {
   serverId = 10;
   serverStatus='offline'
-  // string interpolation using a function
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline'; // 50/50 the status will be either online or offline
+  }
+
+    // string interpolation using a function
   getServerStatus(){
     return this.serverStatus;
+  }
+
+  getColor () {
+    return this.serverStatus === 'online'? 'green' : 'red';
   }
 }
